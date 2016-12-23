39d654c48ad2bc46d3d212f7a659d9113653f43d
NaveenPulamolu <naveenp992@gmail.com>
2016-12-23 13:23:52 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumEx
+{
+    class Program
+    {
+        enum days{
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        };
+        static void Main(string[] args)
+        {
+            int a = (int)days.Sunday;
+            int b = (int)days.Friday;
+            Console.WriteLine("Sunday = {0}", a);
+            Console.WriteLine("Friday = {0}", b);
+            Console.ReadLine();
+        }
+    }
+}
